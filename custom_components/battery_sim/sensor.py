c35bcb2ac2664813c42c8e37d400a12cca8f9a41
dewi-ny-je <2866139+dewi-ny-je@users.noreply.github.com>
2026-03-29 22:49:59 +0200

    Harden restored sensor state parsing

--- custom_components/battery_sim/sensor.py
+++ custom_components/battery_sim/sensor.py
@@ -64,6 +64,7 @@ from .const import (
 )
 
 _LOGGER = logging.getLogger(__name__)
+_INVALID_RESTORED_STATES = {None, "", STATE_UNKNOWN, STATE_UNAVAILABLE}
 
 DEVICE_CLASS_MAP = {
     UnitOfEnergy.WATT_HOUR: SensorDeviceClass.ENERGY,
@@ -207,18 +208,29 @@ class DisplayOnlySensor(RestoreEntity, SensorEntity):
         state = await self.async_get_last_state()
 
         if state:
-            try:
-                self._handle._sensors[self._sensor_type] = float(state.state)
-                last_reset = state.attributes.get(ATTR_LAST_RESET)
-                if self._supports_last_reset and last_reset is not None:
-                    parsed_last_reset = dt_util.parse_datetime(last_reset)
-                    if parsed_last_reset is not None:
-                        self._last_reset = dt_util.as_utc(parsed_last_reset)
-                self._available = True
-                await self.async_update_ha_state(True)
-            except Exception:
-                _LOGGER.debug("Sensor state not restored properly.")
-                self._available = False
+            if state.state in _INVALID_RESTORED_STATES:
+                _LOGGER.debug(
+                    "Ignoring invalid restored state '%s' for sensor '%s'.",
+                    state.state,
+                    self._sensor_type,
+                )
+            else:
+                try:
+                    self._handle._sensors[self._sensor_type] = float(state.state)
+                    last_reset = state.attributes.get(ATTR_LAST_RESET)
+                    if self._supports_last_reset and last_reset is not None:
+                        parsed_last_reset = dt_util.parse_datetime(last_reset)
+                        if parsed_last_reset is not None:
+                            self._last_reset = dt_util.as_utc(parsed_last_reset)
+                    self._available = True
+                    await self.async_update_ha_state(True)
+                except (TypeError, ValueError):
+                    _LOGGER.debug(
+                        "Sensor state '%s' not restored properly for '%s'.",
+                        state.state,
+                        self._sensor_type,
+                    )
+                    self._available = False
         else:
             _LOGGER.debug("No sensor state - presume new battery.")
             self._available = False
@@ -364,9 +376,23 @@ class SimulatedBattery(RestoreEntity, SensorEntity):
 
         state = await self.async_get_last_state()
         if state:
-            self.handle._charge_state = min(
-                float(state.state), self.handle.current_max_capacity
-            )
+            if state.state in _INVALID_RESTORED_STATES:
+                _LOGGER.debug(
+                    "Ignoring invalid restored battery state '%s' for '%s'.",
+                    state.state,
+                    self._name,
+                )
+            else:
+                try:
+                    self.handle._charge_state = min(
+                        float(state.state), self.handle.current_max_capacity
+                    )
+                except (TypeError, ValueError):
+                    _LOGGER.debug(
+                        "Battery state '%s' not restored properly for '%s'.",
+                        state.state,
+                        self._name,
+                    )
             if ATTR_DATE_RECORDING_STARTED in state.attributes:
                 self.handle._date_recording_started = state.attributes[
                     ATTR_DATE_RECORDING_STARTED
